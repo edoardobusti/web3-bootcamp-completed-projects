02cbd2fb152d5afc87045e4392132d1259fc8faf
Edoardo Busti <edobusti.dev@gmail.com>
2025-02-05 20:46:10 +0100

    Add missing Token Sale contract

--- module-1/ERC20TokenSale.sol
+++ module-1/ERC20TokenSale.sol
@@ -5,22 +5,45 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract Token is ERC20Capped {
 
-    event Buy(address buyer, uint256 amount);
-    event SellBack(address seller, uint256 amount);
-    event Withdraw(uint256 amount);
-
     address public immutable owner;
-    uint public immutable tokenSalePrice;
-    uint public mintedSupply;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "not owner");
         _;
     }
 
-    constructor() ERC20("Token", "TOKEN") ERC20Capped(1000 * 10 ** 18) {
+    constructor(address _owner) ERC20("Token", "TOKEN") ERC20Capped(1000 * 10**18) {
+        owner = _owner;
+    }
+
+    function mint(address to, uint256 value) external onlyOwner {
+        _mint(to,value);
+    }
+}
+
+contract TokenSale {
+    event Buy(address buyer, uint256 amount);
+    event SellBack(address seller, uint256 amount);
+    event Withdraw(uint256 amount);
+    event DebugValue(string key, uint256 value);
+
+
+    address public immutable owner;
+    uint256 public immutable tokenSalePrice;
+    uint256 public mintedSupply;
+
+    Token public token;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+
+    constructor() {
         owner = msg.sender;
-        tokenSalePrice = 1 ether; 
+        tokenSalePrice = 1 ether;
+        token = new Token(address(this));
     }
 
     receive() external payable {
@@ -28,60 +51,63 @@ contract Token is ERC20Capped {
     }
 
     function buy() public payable {
-        require(mintedSupply < cap(), "maximum supply reached");
         require(msg.value > 0, "value should not be zero");
-        
-        uint256 tokensToMint = (msg.value * 1000 ether) / tokenSalePrice; 
 
-        require(mintedSupply + tokensToMint <= cap(), "would exceed max supply");
 
-        uint256 contractTokenBalance = balanceOf(address(this));    
+        uint256 tokensToMint = (msg.value * 1000 ether) / tokenSalePrice;
+
+        require(tokensToMint + mintedSupply <= token.cap(), "minting would exceed the cap");
+
+        uint256 contractTokenBalance = token.balanceOf(address(this));
 
         uint256 tokensToTransfer = 0;
 
-
         if (contractTokenBalance > 0) {
             if (contractTokenBalance >= tokensToMint) {
-                tokensToTransfer = tokensToMint; 
+                tokensToTransfer = tokensToMint;
             } else {
-                tokensToTransfer = contractTokenBalance; 
+                tokensToTransfer = contractTokenBalance;
             }
-            
 
-            
-            tokensToMint -= tokensToTransfer; 
-            _transfer(address(this), msg.sender, tokensToTransfer); 
-            mintedSupply += tokensToTransfer; 
+            tokensToMint -= tokensToTransfer;
+            token.transfer(msg.sender, tokensToTransfer);
+            mintedSupply += tokensToTransfer;
         }
 
         if (tokensToMint > 0) {
-            _mint(msg.sender, tokensToMint);
+            token.mint(msg.sender, tokensToMint);
             mintedSupply += tokensToMint;
         }
-        
+
         emit Buy(msg.sender, tokensToTransfer + tokensToMint);
     }
 
     function sellBack(uint256 _amount) external {
         require(_amount > 0, "value should not be zero");
-        require(allowance(msg.sender, address(this)) >= _amount, "contract not approved as spender");
+        require(token.allowance(msg.sender, address(this)) >= _amount, "contract not approved as spender");
 
-        uint256 cashback = (_amount * tokenSalePrice / 0.5 ether) / 1000 ether;
 
-        require(address(this).balance >= cashback);
-        transferFrom(msg.sender, address(this), _amount);
+        uint256 cashback = (_amount * tokenSalePrice) / (2 ether * 1000);
+
+
+
+        require(
+            address(this).balance >= cashback,
+            "insufficient balance for refund"
+        );
+        token.transferFrom(msg.sender, address(this), _amount);
 
         payable(msg.sender).transfer(cashback);
-        
+
         emit SellBack(msg.sender, _amount);
+        emit DebugValue("Cashback Calculated", cashback);
     }
 
     function withdraw() external onlyOwner {
-
         uint256 balance = address(this).balance;
-        
+
         payable(owner).transfer(balance);
-        
-        emit Withdraw(balance);        
+
+        emit Withdraw(balance);
     }
 }
